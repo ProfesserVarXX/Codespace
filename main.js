fd6dce2d47dca0ed1c10f4d46432612cb5746235
ProgrammerVar <powellmatthew442@gmail.com>
2022-03-20 04:12:28 +0000

    upgrade

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let defaults = 0;
 passengers.innerHTML = "Total: " + defaults;
 
 function increment() {
-    let increments = defaults+=1000000000;
+    let increments = defaults+=1``;
 
     passengers.innerHTML = "Total: " + increments;
 } 
